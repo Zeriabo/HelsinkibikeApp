f263b017c5eef32f7836c9832da83bc000b9965c
Zeriab Chiah <zeriab@hotmail.com>
2023-04-17 23:23:25 +0300

    CSVService optimize

--- backend/src/main/java/com/ex/app/services/CSVService.java
+++ backend/src/main/java/com/ex/app/services/CSVService.java
@@ -1,62 +1,54 @@
 package com.ex.app.services;
 
+import com.ex.app.helper.CSVHelper;
+import com.ex.app.models.Asemat;
+import com.ex.app.models.Journey;
+import com.ex.app.repository.AsematRepository;
+import com.ex.app.repository.JourneyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import com.ex.app.helper.CSVHelper;
-import com.ex.app.models.Asemat;
-import com.ex.app.models.Journey;
-import com.ex.app.repository.AsematRepository;
-import com.ex.app.repository.JourneyRepository;
-
 @Service
 public class CSVService {
 
-	 @Autowired
-	 JourneyRepository journeyRepository;
-	 @Autowired
-	 AsematRepository asematRepository;
+    private final JourneyRepository journeyRepository;
+    private final AsematRepository asematRepository;
 
-	  public void saveJourneys(File file) throws ParseException {
-		    try {
-		    	//Don't import journeys that lasted for less than ten seconds
-		    	//Don't import journeys that covered distances shorter than 10 meters
-		    	FileInputStream fis= new FileInputStream(file);
-		    	try {
-		    		 List<Journey> journeys = CSVHelper.csvToJourneys(fis); 
-		    		 journeyRepository.saveAll(journeys);
-		    	}catch (Exception e) {
-		    		throw new RuntimeException("fail to store csv data: " + e.getMessage() + e.getCause());
-			    }
-		     
-		      
-		    } catch (IOException e) {
-		      throw new RuntimeException("fail to store csv data: " + e.getMessage());
-		    }
-		  }
+    @Autowired
+    public CSVService(JourneyRepository journeyRepository, AsematRepository asematRepository) {
+        this.journeyRepository = journeyRepository;
+        this.asematRepository = asematRepository;
+    }
 
-	  public void saveAsemat(File file) throws ParseException {
-		    try {
-		    	
-		    	FileInputStream fis= new FileInputStream(file);
-		    	try {
-		    		 List<Asemat> asemat = CSVHelper.csvToAsemat(fis);
-		    		 asematRepository.saveAll(asemat);
-		    	}catch (Exception e) {
-		    		throw new RuntimeException("fail to store csv data: " + e.getMessage() + e.getCause());
-			    }
-		     
-		      
-		    } catch (IOException e) {
-		      throw new RuntimeException("fail to store csv data: " + e.getMessage());
-		    }
-		  }
-		  public List<Journey> getAllJourneys() {
-		    return journeyRepository.findAll();
-		  }
+    public void saveJourneys(File file) throws ParseException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            List<Journey> journeys = CSVHelper.csvToJourneys(fis);
+
+            // Filter out journeys that lasted for less than 10 seconds or covered distances shorter than 10 meters
+            journeys.removeIf(journey -> journey.getDuration() < 10 || journey.getCoveredDistance() < 10);
+
+            journeyRepository.saveAll(journeys);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to store CSV data: " + e.getMessage(), e);
+        }
+    }
+
+    public void saveAsemat(File file) throws ParseException {
+        try (FileInputStream fis = new FileInputStream(file)) {
+            List<Asemat> asemat = CSVHelper.csvToAsemat(fis);
+            asematRepository.saveAll(asemat);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to store CSV data: " + e.getMessage(), e);
+        }
+    }
+
+    public List<Journey> getAllJourneys() {
+        return journeyRepository.findAll();
+    }
 }
