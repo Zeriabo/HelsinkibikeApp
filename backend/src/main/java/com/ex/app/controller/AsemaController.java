2fcb7a8ea916cbab0385e8d34e79ff2ec47baf77
Zeriab Chiah <zeriab@hotmail.com>
2023-04-17 23:31:26 +0300

    AsemaController optimize

--- backend/src/main/java/com/ex/app/controller/AsemaController.java
+++ backend/src/main/java/com/ex/app/controller/AsemaController.java
@@ -1,120 +1,99 @@
+
 package com.ex.app.controller;
 
 import java.io.File;
 import java.util.List;
-import java.util.Optional;
 
+import com.ex.app.exceptions.InputException;
+import com.ex.app.models.Asemat;
+import com.ex.app.models.ResponseMessage;
+import com.ex.app.services.AsemaService;
+import com.ex.app.services.CSVService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.ex.app.exceptions.InputException;
-import com.ex.app.models.Asemat;
-import com.ex.app.models.Journey;
-import com.ex.app.models.ResponseMessage;
-import com.ex.app.services.AsemaService;
-import com.ex.app.services.CSVService;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
-@RequestMapping("/asema")
 @RestController
+@RequestMapping("/asema")
 public class AsemaController {
 
-	ClassPathResource resource = new ClassPathResource("Helsingin_ja_Espoon_kaupunkipy_asemat_avoin.csv");
-	@Autowired
-	CSVService fileService;
-	
-	@Autowired
-	AsemaService asemaService;
+    private final ClassPathResource resource = new ClassPathResource("Helsingin_ja_Espoon_kaupunkipy_asemat_avoin.csv");
 
-public AsemaController()
-{
-	
-}
-	@PostMapping("/")
-	public ResponseEntity<Asemat> createAsema(@RequestBody Object  asema) throws Exception {
-		
-		
-		ModelMapper modelMapper = new ModelMapper();
-		Asemat newAsema = modelMapper.map(asema, Asemat.class);
-    if(newAsema.getAdres()==null  || newAsema.getKaupunki()==null || newAsema.getName()==null ||newAsema.getNamn()==null || newAsema.getNimi() ==null 
-    		|| newAsema.getOperator()==null || newAsema.getOsoite()== null ||newAsema.getStad()==null )
-    {
-    	throw new InputException("Wrong input!");
+    private final CSVService fileService;
+    private final AsemaService asemaService;
+
+    @Autowired
+    public AsemaController(CSVService fileService, AsemaService asemaService) {
+        this.fileService = fileService;
+        this.asemaService = asemaService;
     }
-		try {
-			
-				return ResponseEntity.status(HttpStatus.OK).body(asemaService.createAsema(newAsema));
-		} catch (Exception e) {
-		
-			throw e;
-		}
 
-	}
-	
-	@PostMapping("/upload")
-	public ResponseEntity<ResponseMessage> uploadFile() {
+    @PostMapping("/")
+    public ResponseEntity<Asemat> createAsema(@RequestBody Object asema) throws Exception {
 
-		String message = "";
-		try {
-			File file = resource.getFile();
-			if(!file.exists())
-			{
-				message="File doesn't exists !"+file.getAbsolutePath();
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage(message));
-			}
-       
-			fileService.saveAsemat(file);
+        ModelMapper modelMapper = new ModelMapper();
+        Asemat newAsema = modelMapper.map(asema, Asemat.class);
 
+        if (newAsema.getAdres() == null || newAsema.getKaupunki() == null || newAsema.getName() == null || newAsema.getNamn() == null || newAsema.getNimi() == null
+                || newAsema.getOperator() == null || newAsema.getOsoite() == null || newAsema.getStad() == null) {
+            throw new InputException("Wrong input!");
+        }
 
-			message = "Uploaded the file successfully: " + file.getName();
-	
-			
-			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-		} catch (Exception e) {
-			message = "Error: "+e.getMessage();
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-		}
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(asemaService.createAsema(newAsema));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 
-	}
-	
-	@GetMapping("/")
-	public ResponseEntity<Page<Asemat>> getStations(@RequestParam(defaultValue = "1") Integer page,@RequestParam(defaultValue = "id")  String sortedBy) {
+    @PostMapping("/upload")
+    public ResponseEntity<ResponseMessage> uploadFile() {
 
+        String message = "";
 
-		try {
-			
-			return ResponseEntity.status(HttpStatus.OK).body(asemaService.getAllStations(sortedBy, page));
-		} catch (Exception e) {
-		
-			throw e;
-		}
+        try {
+            File file = resource.getFile();
 
-	}
-	@GetMapping("/sorted")
-	public ResponseEntity<Page<Asemat>> getJourneysSorted(@RequestParam(defaultValue = "1") Integer page,@RequestParam String sortedBy) {
+            if (!file.exists()) {
+                message = "File doesn't exist! " + file.getAbsolutePath();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseMessage(message));
+            }
 
-		try {
+            fileService.saveAsemat(file);
 
-			return ResponseEntity.status(HttpStatus.OK).body(asemaService.getAllStations(sortedBy, page));
-		} catch (Exception e) {
+            message = "Uploaded the file successfully: " + file.getName();
+            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+        } catch (Exception e) {
+            message = "Error: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+        }
+    }
 
-			throw e;
-		}
+    @GetMapping("/")
+    public ResponseEntity<Page<Asemat>> getStations(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "id") String sortedBy) {
 
-	}
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(asemaService.getAllStations(sortedBy, page));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @GetMapping("/sorted")
+    public ResponseEntity<Page<Asemat>> getJourneysSorted(@RequestParam(defaultValue = "1") Integer page, @RequestParam String sortedBy) {
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(asemaService.getAllStations(sortedBy, page));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
 
 	@GetMapping("/search_capacity")
 	public ResponseEntity<Page<Asemat>> getStationsByCapacity(@RequestParam String search) {
